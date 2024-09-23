3940f417538a20e4fae1021cef3af225280722ab
Mathu21DTU <160587088+Mathu21DTU@users.noreply.github.com>
2024-09-23 14:25:46 +0200

    hi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ifcopenshell
 from .rules import windowRule
 from .rules import doorRule
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open("/Users/bruger/Downloads/CES_BLD_24_06_MEP.ifc")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
