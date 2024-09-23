21d7841844e0cad704f32a370b5a4d5ac30f1a1b
Mathu21DTU <160587088+Mathu21DTU@users.noreply.github.com>
2024-09-23 16:34:11 +0200

    Updated_Works

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import ifcopenshell
 
 from rules import AHURule
 # Vi importerer filen via ifcopenshell
-model = ifcopenshell.open("\\Users\\bruger\\Downloads\\CES_BLD_24_06_MEP.IFC")
+model = ifcopenshell.open("/Users/bruger/Downloads/CES_BLD_24_06_MEP.ifc")
 
 # Vi definerer AHU:
-AHUResult = windowRule.checkRule(model)
+AHUResult = AHURule.checkRule(model)
 
 # Vi tjekker hvor mange AHU der er via:
 print("AHU result:",AHUResult)
--- rules/AHURule.py
+++ rules/AHURule.py
@@ -1,7 +1,7 @@
 import ifcopenshell
 
 def checkRule(model):
-    AHU = model.by_type('fcBuildingElementProx')
+    AHU = model.by_type('ifcBuildingElementProxy')
 
     result = f"There are: {len(AHU)} air handling units."
 
